5dad92298699c79085fd09e14987a796a83697f1
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-12 17:45:13 +0100

    feat: Generate random number between -10000 and 10000 and print the last digit and if its greater than 5 or less than 6 and if is zero or not zero

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+
+def main():
+    last_digit = number % 10
+
+    end = ""
+
+    is_zero = "and is not zero"
+    if last_digit == 0:
+        is_zero = "and not zero"
+        end = is_zero
+    else:
+        greater_or_less = ""
+        if last_digit > 5:
+            greater_or_less = "is greater than 5"
+        elif last_digit < 6:
+            greater_or_less = f"is less than 6 {is_zero}"
+
+        end = greater_or_less
+
+    print(f"Last digit of {number} is {last_digit} {end}")
+
+
+if __name__ == "__main__":
+    main()
