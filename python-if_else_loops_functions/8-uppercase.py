44768417759b70b1ec367743579ec669918974fc
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-13 11:02:17 +0100

    feat: Added uppercase function print an str but uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    result = ""
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            result += chr(ord(str[i]) - 32)
+        else:
+            result += str[i]
+
+    print(result)
