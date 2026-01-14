f000850ed25939cabf75d04aacac5f0b6d118b78
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-14 14:54:56 +0100

    Added .format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 for i in range(9):
     for y in range(10):
         if y > i:
-            print(f"{i}{y}{", " if f"{i}{y}" != "89" else "\n"}", end="")
+            end = ", " if f"{i}{y}" != "89" else "\n"
+            print("{}{}{}".format(i, y, end), end="")
