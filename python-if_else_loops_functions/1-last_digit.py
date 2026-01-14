7bcaa07f2be3dac7b9847ed6988640e4ca612a0c
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-14 14:32:01 +0100

    Added support of negative number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 
-
 def main():
     last_digit = number % 10
+    if number < 0:
+        last_digit = ((number * -1) % 10 * -1)
 
     end = ""
 
