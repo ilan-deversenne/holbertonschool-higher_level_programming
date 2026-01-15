700af2c557d65ad087e21fd2073a32e93a4e860a
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-15 23:31:16 +0100

    fix: Added main guard

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 c = __import__("calculator_1")
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-for op, func in {"+": c.add, "-": c.sub, "*": c.mul, "/": c.div}.items():
-    print("{} {} {} = {}".format(a, op, b, func(a, b)))
+    for op, func in {"+": c.add, "-": c.sub, "*": c.mul, "/": c.div}.items():
+        print("{} {} {} = {}".format(a, op, b, func(a, b)))
