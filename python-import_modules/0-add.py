c4ab5a9a4ef4103e162edb8c46a67d7ab6a1d156
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-15 23:24:39 +0100

    fix: Changed import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-add = __import__("add_0").add
 
 
 def main():
     a = 1
     b = 2
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, __import__("add_0").add(a, b)))
 
 
 if __name__ == "__main__":
