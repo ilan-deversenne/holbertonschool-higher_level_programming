15d148aa139331610416c8b5caaf85e8a42f6e88
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-15 23:28:16 +0100

    fix: Removed main function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
-
-def main():
+if __name__ == "__main__":
     a = 1
     b = 2
-
     print("{} + {} = {}".format(a, b, __import__("add_0").add(a, b)))
-
-
-if __name__ == "__main__":
-    main()
