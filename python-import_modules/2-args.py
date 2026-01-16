583b43cf3c0ff08b22f2156b23f5fc77d96fe27c
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-16 12:45:56 +0100

    fix: Added main guard

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-argc = len(sys.argv)
-end = ":" if argc > 0 else "."
-print("{} argument{}{}".format(argc - 1, "" if argc == 1 else "s", end))
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    end = ":" if argc > 0 else "."
+    print("{} argument{}{}".format(argc - 1, "" if argc == 1 else "s", end))
 
-for i in range(1, argc):
-    print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, argc):
+        print("{}: {}".format(i, sys.argv[i]))
