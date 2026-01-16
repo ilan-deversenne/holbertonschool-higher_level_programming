a389f9bcb593f739612ce1b265bf95eae1449e71
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-16 12:48:33 +0100

    fix: Added -1 to argc in "s" display condition

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     argc = len(sys.argv)
     end = ":" if argc > 0 else "."
-    print("{} argument{}{}".format(argc - 1, "" if argc == 1 else "s", end))
+    print("{} argument{}{}".format(argc - 1, "" if argc - 1 == 1 else "s", end))
 
     for i in range(1, argc):
         print("{}: {}".format(i, sys.argv[i]))
