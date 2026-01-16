79f7a743a0e822fd4bdecd71f852eec4f9900ef6
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-16 12:41:40 +0100

    fix: s only if argc is not equal to 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 import sys
 
 argc = len(sys.argv)
-print("{} arguments{}".format(argc - 1, ":" if argc > 0 else "."))
+end = ":" if argc > 0 else "."
+print("{} argument{}{}".format(argc - 1, "" if argc == 1 else "s", end))
 
 for i in range(1, argc):
     print("{}: {}".format(i, sys.argv[i]))
