5d9803fe8de9eabec5d50f3d4eed1aa79c5c8b13
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-16 13:15:57 +0100

    fix: Added s variable for reduce size of line

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,8 +3,9 @@ import sys
 
 if __name__ == "__main__":
     argc = len(sys.argv)
+    s = "" if argc - 1 == 1 else "s"
     end = ":" if argc > 0 else "."
-    print("{} argument{}{}".format(argc - 1, "" if argc - 1 == 1 else "s", end))
+    print("{} argument{}{}".format(argc - 1, s, end))
 
     for i in range(1, argc):
         print("{}: {}".format(i, sys.argv[i]))
