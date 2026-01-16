a3af3ad46699f5f383c03f4f3c2d7665474713c6
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-16 12:37:32 +0100

    fix: Use .format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-argc = len(sys.argv) - 1
-print(f"{argc} arguments{":" if argc > 0 else "."}")
+argc = len(sys.argv)
+print("{} arguments{}".format(argc - 1, ":" if argc > 0 else "."))
 
-for i in range(1, argc + 1):
-    print(f"{i}: {sys.argv[i]}")
+for i in range(1, argc):
+    print("{}: {}".format(i, sys.argv[i]))
