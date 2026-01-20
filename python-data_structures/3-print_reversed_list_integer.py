80be6f5e4c2fd1cec59fe5f60583adb76dc62c13
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-01-20 14:46:54 +0100

    fix: Reverse list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for el in my_list:
-        print(el)
+    for idx in range(0, len(my_list)):
+        print("{:d}".format(my_list[len(my_list) - idx - 1]))
