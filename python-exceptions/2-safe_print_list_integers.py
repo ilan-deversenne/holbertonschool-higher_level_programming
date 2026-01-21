dade4d8f92081611f87378a82a762600c632ad45
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-01-21 18:49:24 +0100

    Added safe_print_list_integers function
    Safe print a list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,10 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[i]), end="")
             printed += 1
         except:
             pass
+    print()
 
     return printed
