1db8f96214515c3afe420ca0713a78a1a5c32795
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-01-22 20:10:54 +0100

    Changed except logic

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,6 +2,10 @@
 
 def safe_print_division(a, b):
     try:
-        return a / b
+        result = a / b
     except:
         pass
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
