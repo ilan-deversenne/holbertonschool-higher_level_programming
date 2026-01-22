09889ca3da46b08eb67b8770ac695ac89b922e84
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-01-22 20:14:50 +0100

    Revert "fix: Juste return None if execpt"
    This reverts commit c8b82ff428bf1c19a7027ca626114cb9c3d8b3f6.

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    result = None
+
     try:
         result = a / b
     except:
-        return None
+        pass
     finally:
         print("Inside result: {}".format(result))
 
