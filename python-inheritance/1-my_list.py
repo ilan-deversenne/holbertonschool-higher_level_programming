6a6d0616800b9d5abe31d08a1157204cbfc6b268
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-02 11:41:11 +0100

    Added docstring

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""
+    MyList is custom list class that inheritance from list
+"""
+
 
 class MyList(list):
     def __init__(self):
         pass
-    
+
+    """
+        Print sorted list
+    """
     def print_sorted(self):
         print(sorted(self))
