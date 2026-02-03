771b8f5af7574bb4fbd68d3a00ae8bed793d710b
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-03 14:28:50 +0100

    Added Animal class inherits from ABC, Dog and Cat class inherits from Animal

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+
+"""
+Animal class
+"""
+
+
+class Animal(ABC):
+    """
+    Init animal
+    """
+    def __init__(self):
+        super().__init__()
+
+    """
+    Return string of animal sound
+    """
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+"""
+Dog class
+"""
+
+
+class Dog(Animal):
+    """
+    Init dog
+    """
+    def __init__(self):
+        super().__init__()
+
+    """
+    Return string of animal sound
+    """
+    def sound(self):
+        return "Bark"
+
+
+"""
+Cat class
+"""
+
+
+class Cat(Animal):
+    """
+    Init cat
+    """
+    def __init__(self):
+        super().__init__()
+
+    """
+    Return string of animal sound
+    """
+    def sound(self):
+        return "Meow"
