b04a63264b862310106d18da33fd47edd18ed81c
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-04 11:53:32 +0100

    fix: Replace not equals to is not

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 """
 Check if the object is an instance of a class that inherited from given class
 """
 
 
 def inherits_from(obj, a_class):
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
