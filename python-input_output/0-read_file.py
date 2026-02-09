9bfa8494938a01efa2dfeb97566871167d147f54
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-09 13:50:54 +0100

    feat: Added read_file function
    Print content of file with gitven filename

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Print content of file with his filename
+"""
+
+
+def read_file(filename=""):
+    """
+    Docstring for read_file
+
+    :param filename: Filename
+    """
+    with open(filename, 'r') as f:
+        print(f.read())
