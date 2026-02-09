a152a5bdb6163fd4b7a27d22633adf378c2cd552
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-09 14:26:50 +0100

    feat: Added to_json_string function
    Convert object to a json string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from json import dumps
+
+"""
+Object to json string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Docstring for to_json_string
+
+    :param my_obj: Object
+    """
+
+    return dumps(my_obj)
