3b4849c0e01340b09579f3fb0d2fd0df885df91f
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-10 13:52:06 +0100

    fix: Removed blank line & added a new line for pycodestyle

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,10 +5,11 @@ from json import dumps
 Convert a Class to JSON
 """
 
+
 def class_to_json(obj: object) -> dict:
     """
     Docstring for class_to_json
-    
+
     :param obj: Object
     """
 
