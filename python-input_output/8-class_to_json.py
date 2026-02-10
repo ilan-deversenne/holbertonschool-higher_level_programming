6d3cd38c1ffad972e0fb9d1f6238a04fa206a64c
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-10 13:47:54 +0100

    feat: Added class_to_json function
    convert class to json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from json import dumps
+
+"""
+Convert a Class to JSON
+"""
+
+def class_to_json(obj: object) -> dict:
+    """
+    Docstring for class_to_json
+    
+    :param obj: Object
+    """
+
+    return obj.__dict__
