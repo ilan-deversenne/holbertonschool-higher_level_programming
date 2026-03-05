624b68f66ab94022ef1d7203455ab83de9ead97f
Ilan <ilan.deversenne@holbertonstudents.com>
2026-03-05 16:11:22 +0100

    fix: get id, name only not all

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- List cities where state is California with a sub query
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
