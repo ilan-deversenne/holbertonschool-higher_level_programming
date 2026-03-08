2ded4d8fe58e47f6d4c8db52f740dd66349d738d
Ilan <ilan.deversenne@holbertonstudents.com>
2026-03-08 21:54:52 +0100

    Fetch all states where name equals to argv 4

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+Fetch all states where name equals to argv 4
+"""
+
+import MySQLdb
+import sys
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    name = sys.argv[4].replace("'", '')
+
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (name,))
+
+    for row in c.fetchall():
+        print(row)
+
+    c.close()
+    db.close()
+
+if __name__ == '__main__':
+    main()
