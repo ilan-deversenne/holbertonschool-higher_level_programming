6c8e9a6622fc7ec84bd468d9c7659ecd7ab950ae
Ilan <ilan.deversenne@holbertonstudents.com>
2026-03-08 17:15:27 +0100

    fix: I missed AS genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- List the number of shows by genre
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.id
