4cadb90d7f21394060dc6c5c5228e02977697a2f
Ilan <ilan.deversenne@holbertonstudents.com>
2026-03-08 21:09:43 +0100

    fix: Try to add states. before id

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ db = MySQLdb.connect(
 )
 
 c = db.cursor()
-c.execute("SELECT * FROM states ORDER BY id ASC")
+c.execute("SELECT * FROM states ORDER BY states.id ASC")
 
 for row in c.fetchall():
     print(row)
