69896240dcd7a8155a0a720a7b9dacedcb50f989
Ilan <ilan.deversenne@holbertonstudents.com>
2026-03-08 21:16:29 +0100

    fix: Better readability of code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,12 +3,16 @@
 import MySQLdb
 import sys
 
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
 db = MySQLdb.connect(
     host="localhost",
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    port=3306
+    port=3306,
+    user=username,
+    passwd=password,
+    db=database
 )
 
 c = db.cursor()
