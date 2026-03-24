6698665b119f005d4cdaf7790765ec2eb1034d12
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-03-24 15:37:15 +0100

    factorial of given number

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const n = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (!n || n == 1) return 1;
+  if (n < 1) return -1;
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(n));
