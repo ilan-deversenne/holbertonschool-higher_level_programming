8a92c11075f1d1964540cc8d1947efe9726e0167
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-03-25 13:15:03 +0100

    add element when #add_item is clicked

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,8 @@
+const list = document.querySelector('.my_list');
+
+document.getElementById('add_item').addEventListener('click', () => {
+    let el = document.createElement('li');
+    el.innerHTML = 'Item';
+
+    list.appendChild(el);
+});
